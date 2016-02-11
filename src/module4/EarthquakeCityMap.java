2c1ae5e69224616a5bae3d955ab536f6a3e34ac7
Lourdes Tupas <lourdes.tupas@gmail.com>
2016-02-11 09:27:16 -0800

    initial changes to EarthquakeCityMap

--- src/module4/EarthquakeCityMap.java
+++ src/module4/EarthquakeCityMap.java
@@ -20,7 +20,7 @@ import processing.core.PApplet;
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
  * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Lourdes Pajarillo
  * Date: July 17, 2015
  * */
 public class EarthquakeCityMap extends PApplet {
@@ -62,7 +62,9 @@ public class EarthquakeCityMap extends PApplet {
 	
 	public void setup() {		
 		// (1) Initializing canvas and map tiles
-		size(900, 700, OPENGL);
+		//size(900, 700, OPENGL);
+		size(900, 700);
+		
 		if (offline) {
 		    map = new UnfoldingMap(this, 200, 50, 650, 600, new MBTilesMapProvider(mbTilesString));
 		    earthquakesURL = "2.5_week.atom";  // The same feed, but saved August 7, 2015
@@ -92,7 +94,7 @@ public class EarthquakeCityMap extends PApplet {
 		List<Feature> cities = GeoJSONReader.loadData(this, cityFile);
 		cityMarkers = new ArrayList<Marker>();
 		for(Feature city : cities) {
-		  cityMarkers.add(new CityMarker(city));
+			cityMarkers.add(new CityMarker(city));
 		}
 	    
 		//     STEP 3: read in earthquake RSS feed
@@ -100,15 +102,15 @@ public class EarthquakeCityMap extends PApplet {
 	    quakeMarkers = new ArrayList<Marker>();
 	    
 	    for(PointFeature feature : earthquakes) {
-		  //check if LandQuake
-		  if(isLand(feature)) {
-		    quakeMarkers.add(new LandQuakeMarker(feature));
-		  }
-		  // OceanQuakes
-		  else {
-		    quakeMarkers.add(new OceanQuakeMarker(feature));
-		  }
-	    }
+	    	//check if LandQuake
+			if(isLand(feature)) {
+				quakeMarkers.add(new LandQuakeMarker(feature));
+			}
+			// OceanQuakes
+			else {
+				quakeMarkers.add(new OceanQuakeMarker(feature));
+			}
+		}
 
 	    // could be used for debugging
 	    printQuakes();
@@ -125,8 +127,7 @@ public class EarthquakeCityMap extends PApplet {
 	public void draw() {
 		background(0);
 		map.draw();
-		addKey();
-		
+		addKey();		
 	}
 	
 	// helper method to draw key in GUI
@@ -163,9 +164,12 @@ public class EarthquakeCityMap extends PApplet {
 	private boolean isLand(PointFeature earthquake) {
 		
 		// IMPLEMENT THIS: loop over all countries to check if location is in any of them
-		
-		// TODO: Implement this method using the helper method isInCountry
-		
+		for (Marker country : countryMarkers) {
+			if (isInCountry(earthquake,country)) {
+				earthquake.setLocation(country.getLocation());
+				return true;
+			}
+		}
 		// not inside any country
 		return false;
 	}
@@ -179,6 +183,23 @@ public class EarthquakeCityMap extends PApplet {
 	private void printQuakes() 
 	{
 		// TODO: Implement this method
+		for (Marker country : countryMarkers) {
+			int quakeCtr = 0;
+			
+			for(Marker quakeMarker : quakeMarkers) {
+				if (quakeMarker instanceof LandQuakeMarker) {
+					LandQuakeMarker landMarker = (LandQuakeMarker) quakeMarker; 
+					if (landMarker.getCountry() == country.toString()) {
+						quakeCtr++;
+						break;
+					}					
+				}
+			}
+			
+			if (quakeCtr > 0) {
+				System.out.println(country.toString()+"\t"+quakeCtr);
+			}
+		}
 	}
 	
 	
